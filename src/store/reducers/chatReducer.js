0e41b6310a78ce48c2c7e8897dc6ba7d177b7d49
Vera <venera.wow@gmail.com>
2023-05-25 11:00:18 +0600

    added send message logic, added UserMessage component

--- src/store/reducers/chatReducer.js
+++ src/store/reducers/chatReducer.js
@@ -1,6 +1,17 @@
+import actionTypes from '../../constants/actionTypes';
+
 const initialState = {
+    messages: [],
 };
 
 export default function chatReducer(state = initialState, { type, payload }) {
-    return state
+    switch (type) {
+        case actionTypes.SET_USERS_MESSAGES_STORE:
+            return {
+                ...state,
+                messages: [...state.messages, ...payload],
+            };
+        default:
+            return state
+    }
 };
